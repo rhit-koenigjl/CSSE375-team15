f31696929f9098aff83d6300ddf590a701b97f98
JL Koenig <koenigjl@rose-hulman.edu>
2024-04-29 14:03:34 -0400

    added menu specific images to MenuImage

--- src/main/java/arcadeGame/MenuImage.java
+++ src/main/java/arcadeGame/MenuImage.java
@@ -9,7 +9,12 @@ public enum MenuImage {
   LOGO("menu_logo.png", (int) (940 * 0.65), (int) (384 * 0.65)),
   PLAY("play_button.png", (int) (72 * 1.7), (int) (108 * 1.7)),
   HELP("help_button.png", (int) (48 * 1.7), (int) (72 * 1.7)),
-  CREDITS("credits_button.png", (int) (48 * 1.7), (int) (72 * 1.7));
+  CREDITS("credits_button.png", (int) (48 * 1.7), (int) (72 * 1.7)),
+  MENU_BLOCK("menu_block.png", 512, 128),
+  LEFT_KEY("arrow_key_left.png", 64, 64),
+  RIGHT_KEY("arrow_key_right.png", 64, 64),
+  UP_KEY("arrow_key_up.png", 64, 64),
+  DOWN_KEY("arrow_key_down.png", 64, 64);
 
   private BufferedImage image;
 
