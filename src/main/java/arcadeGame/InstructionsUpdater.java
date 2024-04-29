ac1056138962e9e22eb914ca5a8e8b10e72e6c88
JL Koenig <koenigjl@rose-hulman.edu>
2024-04-29 13:16:05 -0400

    created instruction screen class

---
+++ src/main/java/arcadeGame/InstructionsUpdater.java
@@ -0,0 +1,21 @@
+package arcadeGame;
+
+public class InstructionsUpdater extends SceneUpdater{
+
+    InstructionsUpdater(SceneManager sceneManager) {
+        super(sceneManager);
+    }
+
+    @Override
+    void updateScene() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateScene'");
+    }
+
+    @Override
+    String getSceneName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSceneName'");
+    }
+    
+}
