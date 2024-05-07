0a06f7d92b685cc61af9b9b2b96e74052e0f8b4c
Canon <22649817+cm090@users.noreply.github.com>
2024-05-07 14:10:57 -0400

    Refactor PlayerJumpSprite

--- src/main/java/arcadeGame/gameComponents/spriteAnimations/PlayerJumpSprite.java
+++ src/main/java/arcadeGame/gameComponents/spriteAnimations/PlayerJumpSprite.java
@@ -3,11 +3,16 @@ package arcadeGame.gameComponents.spriteAnimations;
 import arcadeGame.gameComponents.imageManagers.GameImage;
 
 public class PlayerJumpSprite extends DisplaySprite {
+    private static final int VELOCITY_DIVISOR = 17;
+    private static final int WIDTH_DIVISOR = 30;
+    private static final int X_DIVISOR = 60;
+    private static final int Y_DIVISOR = 120;
+
     private double origWidth;
 
     public PlayerJumpSprite(double x, double y, double width) {
         super(x, y, width, width / 2, GameImage.JUMP_WIND);
-        vy = width / 17;
+        vy = width / VELOCITY_DIVISOR;
         origWidth = width;
     }
 
@@ -15,11 +20,11 @@ public class PlayerJumpSprite extends DisplaySprite {
     public void updatePosition() {
         y += vy;
         if (width > 0) {
-            width -= origWidth / 30;
-            x += origWidth / 60;
+            width -= origWidth / WIDTH_DIVISOR;
+            x += origWidth / X_DIVISOR;
 
-            y += origWidth / 120;
-            height -= origWidth / 60;
+            y += origWidth / Y_DIVISOR;
+            height -= origWidth / X_DIVISOR;
         } else {
             width = 0;
         }
