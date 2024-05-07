a5170f1646f8cdfbcfef3fd4e7093e5560037e37
Canon <22649817+cm090@users.noreply.github.com>
2024-05-07 13:11:14 -0400

    Refactor MainApp

--- src/main/java/arcadeGame/MainApp.java
+++ src/main/java/arcadeGame/MainApp.java
@@ -13,22 +13,37 @@ class MainApp {
     private static final int DELAY = 10;
     private static final int MENU_WIDTH = 900;
     private static final int MENU_HEIGHT = 550;
+    private static final String GAME_TITLE = "Indiana Ghost";
+    private static final Color LIGHT_BLUE = new Color(206, 214, 237);
 
     private GameComponent component;
     private JFrame gameFrame;
     private MouseListener mouseListener;
 
     private void runApp() {
-        gameFrame = new JFrame("Indiana Ghost");
+        createFrame();
+        createGameComponent();
+        addListeners();
+        gameFrame.setVisible(true);
+    }
+    
+    private void createFrame() {
+        gameFrame = new JFrame(GAME_TITLE);
         gameFrame.setSize(MENU_WIDTH, MENU_HEIGHT);
         this.mouseListener = new MouseListener(gameFrame);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setResizable(false);
+    }
 
+    private void createGameComponent() {
         component = new GameComponent(gameFrame, mouseListener);
         component.loadLevelByIndex(0);
         gameFrame.add(component, BorderLayout.CENTER);
-        gameFrame.getContentPane().setBackground(new Color(206, 214, 237));
-        GameAdvanceListener advanceListener = new GameAdvanceListener(component);
+        gameFrame.getContentPane().setBackground(LIGHT_BLUE);
+    }
 
+    private void addListeners() {
+        GameAdvanceListener advanceListener = new GameAdvanceListener(component);
         Timer timer = new Timer(DELAY, advanceListener);
         timer.start();
 
@@ -50,10 +65,6 @@ class MainApp {
             }
 
         });
-
-        gameFrame.setVisible(true);
-        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameFrame.setResizable(false);
     }
 
     public static void main(String[] args) {
